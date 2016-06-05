aa97f77c2bc9b9ee2bfe00897ed659d17e9865a1
David Savage <David Savage>
2016-06-04 21:47:39 -0500

    Integrated authentication using Azure Authentication Gateway Google is setup for now Add ability to debug locally

--- src/Web/Global.asax.cs
+++ src/Web/Global.asax.cs
@@ -1,13 +1,16 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using StructureMap;
-using Web.App_Start;
 using Web.DependencyResolution;
 
 namespace Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class App : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
@@ -15,6 +18,17 @@ namespace Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters, IoC.Initialize());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        internal static IPrincipal DebugUser { get; set; }
+        
+        protected void Application_AuthenticateRequest()
+        {
+            if (!Config.Settings.UserDebug)
+                return;
+
+            HttpContext.Current.User = DebugUser;
+            Thread.CurrentPrincipal = DebugUser;
         }
     }
 }
